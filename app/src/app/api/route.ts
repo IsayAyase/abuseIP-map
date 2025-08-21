06c1207c463f23ae26048ae9f79a5efbc149f465
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-21 19:11:52 +0530

    Refactor ESLint configuration for improved rule management; update API GET function parameter to underscore for clarity

--- app/src/app/api/route.ts
+++ app/src/app/api/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(_: NextRequest) {
     return Response.json(
         { success: true, message: "API is working!" },
         {
