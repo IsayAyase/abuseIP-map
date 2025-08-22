41ec7f3fe8412b9a2af4da9fc1f552d825ca404e
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-22 13:55:06 +0530

    Refactor abuseIpWithInfo model to include IpInfoType schema; enhance structure and maintainability

--- app/src/db/models/abuseIpWithInfo.ts
+++ app/src/db/models/abuseIpWithInfo.ts
@@ -1,7 +1,103 @@
-import type { AbuseIpWithInfoType } from "@/db/types";
-import mongoose, { type Model } from "mongoose";
+import type { AbuseIpWithInfoType, IpInfoType } from "@/db/types";
+import mongoose from "mongoose";
 
-const AbuseIpWithInfo = mongoose.models
-    .abuseipwithinfo as Model<AbuseIpWithInfoType>;
+const IpInfoTypeSchema = new mongoose.Schema<IpInfoType>(
+    {
+        status: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+        countryCode: {
+            type: String,
+            required: true,
+        },
+        region: {
+            type: String,
+            required: true,
+        },
+        regionName: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        zip: {
+            type: String,
+            required: true,
+        },
+        lat: {
+            type: Number,
+            required: true,
+        },
+        lon: {
+            type: Number,
+            required: true,
+        },
+        timezone: {
+            type: String,
+            required: true,
+        },
+        isp: {
+            type: String,
+            required: true,
+        },
+        query: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        id: false,
+        _id: false,
+        // no id, use your brain, genius!
+    }
+);
+const abuseIpWithInfoSchema = new mongoose.Schema<AbuseIpWithInfoType>(
+    {
+        ipAddress: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        abuseConfidenceScore: {
+            type: Number,
+            required: true,
+        },
+        countryCode: {
+            type: String,
+            required: true,
+        },
+        lastReportedAt: {
+            type: Date,
+            required: true,
+        },
+        ipInfo: {
+            type: IpInfoTypeSchema,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// TTL index: auto-delete docs 3 days after lastReportedAt
+abuseIpWithInfoSchema.index(
+    { lastReportedAt: 1 },
+    {
+        expireAfterSeconds: 3 * 24 * 60 * 60, // 3 days in seconds
+    }
+);
+
+const AbuseIpWithInfo =
+    mongoose.models.abuseipwithinfo ||
+    mongoose.model("abuseipwithinfo", abuseIpWithInfoSchema);
 
 export default AbuseIpWithInfo;
